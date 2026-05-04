5bdf795f040be02c2c05a39ab4ec801a65ad9f9c
MARNOLD14 <mckinziearnold1@gmail.com>
2026-05-04 19:54:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.spinner('Parsing schedule...'):
             st.warning('No shift assignments were found in this file. Make sure you uploaded a ShiftAdmin generated schedule page.')
             st.stop()
 
-        shiftsp = shiftsp.sort_values('userId')
+        shiftsp = shiftsp.sort_values('userName')
 
         st.success(f'Successfully extracted schedule for {len(shiftsp)} providers.')
         st.dataframe(shiftsp, hide_index=True, use_container_width=True)
